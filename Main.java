8cb32552ff4742173bb47ca84ed57f804e140c17
NatalieR2026 <natalie.radin2026@gbwl.org>
2024-11-19 13:56:45 +0000

    first day notes

--- Main.java
+++ Main.java
@@ -1,6 +1,30 @@
 public class Main {
 
    public static void main(String []args) {
-      System.out.println("Hello World");
+      
+      boolean condition = false;
+      // Statements inside a WHILE LOOP BODY will repeat
+      // until the condition is FALSE
+      while (condition) {
+         // Statements (instructions) to be repeated
+         System.out.println("Hi");
+         // While condition is false, the loop body won't execute
+         // If condition was set to true, we'd have an INFINITE LOOP
+      }
+
+      // THREE STEPS TO WRITE A LOOP
+      // 1. Initialize the LOOP CONTROL variable
+      int count = 1;
+      // 2. Test the CONDITION in the loop header
+      // here, the condition is "count <= 10"
+      // but it coould also just be a boolean value
+      while (count <= 10) {
+         // execute statements in the loop body
+         System.out.println(count);
+         // 3. CHANGE your loop control variable
+         count++; // equivalent to count = count + 1;
+      }
+
+
    }
 }
