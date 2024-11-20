d32af1ebe674c0a7935f87b7ff52d63fe8b067e9
NatalieR2026 <natalie.radin2026@gbwl.org>
2024-11-20 14:40:55 +0000

    second day notes

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
    public static void main(String []args) {
@@ -24,6 +26,31 @@ public class Main {
          // 3. CHANGE your loop control variable
          count++; // equivalent to count = count + 1;
       }
+
+      // GUESSING GAME
+      Scanner scan = new Scanner(System.in);
+      // Choose a random number from 0-100
+      int num = (int) (Math.random() * 100);
+      // Ask the user to guess a number from 1 to 100
+      System.out.println("Guess a number from 0 to 100: ");
+      // Get the first guess using scan.nextInt();
+      int guess = scan.nextInt();
+      // Example of a SENTINEL while loop (like a "flag" to stop looping)
+      // Loop while the guess down not equal the random number
+      while (guess != num) {
+         if (guess < num) {
+            System.out.println("Too low!");
+         }
+         else if (guess > num) {
+            System.out.println("Too high!");
+         }
+         // Get a new guess (save it to the same variable)
+         System.out.println("Guess again: ");
+         guess = scan.nextInt(); // LOOP CHANGE
+      }
+      // AFTER LOOP ENDS (meaning guess == num)
+      System.out.println("You got it!");
+
 
 
    }
